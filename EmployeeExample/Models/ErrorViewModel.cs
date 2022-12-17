908794de4cc7fd6dc96571eb2336bd2ab5067b03
travis@travestyofcode.com <travis@travestyofcode.com>
2022-12-17 04:42:16 -0500

    Add project files.

---
+++ EmployeeExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
