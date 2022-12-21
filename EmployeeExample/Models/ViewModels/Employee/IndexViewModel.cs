6b904449d07f2b91b9ad274e2c5fb3595683f1cd
travis@travestyofcode.com <travis@travestyofcode.com>
2022-12-21 15:13:17 -0500

    Adds Employee IndexViewModel to indicate if current user can Edit/Delete the Employee.

---
+++ EmployeeExample/Models/ViewModels/Employee/IndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeExample.Models.ViewModels.Employee
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(1024)]
+        [Display(Name = "Work Email")]
+        public string WorkEmail { get; set; }
+
+        [MaxLength(32)]
+        [Display(Name = "Work Phone")]
+        public string WorkPhone { get; set; }
+        
+        public bool CanDelete { get; set; }
+
+        public bool CanEdit { get; set; }
+    }
+}
